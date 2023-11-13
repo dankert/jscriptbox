64a93363a9054071ddc1b6b0947c60ce405be62e
Jan Dankert <jan.dankert@hansemerkur.de>
2023-11-13 20:50:09 +0100

    Test for type conversions.

---
+++ test/de/openrat/jscriptbox/TypeConversionTest.java
@@ -0,0 +1,27 @@
+package de.openrat.jscriptbox;
+
+import de.openrat.jscriptbox.executor.ScriptInterpreter;
+import junit.framework.TestCase;
+
+public class TypeConversionTest extends TestCase{
+
+    public void testSimpleAddition() throws Exception {
+        ScriptInterpreter interpreter = new ScriptInterpreter();
+
+        interpreter.runCode( "write(1+1);");
+        assertEquals("2",interpreter.getOutput());
+
+        interpreter.runCode( "write(1+'1');");
+        assertEquals("2",interpreter.getOutput());
+    }
+
+    public void testSimpleSubstraction() throws Exception {
+        ScriptInterpreter interpreter = new ScriptInterpreter();
+
+        interpreter.runCode( "write(3-2);");
+        assertEquals("1",interpreter.getOutput());
+
+        interpreter.runCode( "write(3-'2');");
+        assertEquals("1",interpreter.getOutput());
+    }
+}
